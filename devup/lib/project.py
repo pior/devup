f4b83ebf5bfead0a07151b1c7f37f09fc92a9334
Pior Bastida <pior@pbastida.net>
2017-05-21 18:54:24 -0400

    Remove remote_path from Project

--- devup/lib/project.py
+++ devup/lib/project.py
@@ -25,24 +25,23 @@ def list_project_dirs():
 class Project(object):
     """Project as locally cloned repository in source directory."""
 
-    def __init__(self, local_path, remote_path):
-        self.local_path = local_path
-        self.remote_path = remote_path
+    def __init__(self, path):
+        self.path = path
 
     @classmethod
     def by_repo(cls, repo):
-        local_path = project_path_from_id(repo.local_id)
-        return cls(local_path, repo.location)
+        path = project_path_from_id(repo.local_id)
+        return cls(path)
 
     @classmethod
     def by_name(cls, name):
         paths = list_project_dirs()
         for path in paths:
             if path.parts[-1] == name:
-                return cls(path, None)
+                return cls(path)
         for path in paths:
             if path.parts[-1].startswith(name):
-                return cls(path, None)
+                return cls(path)
 
     def exists(self):
         return os.path.exists(self.local_path)
